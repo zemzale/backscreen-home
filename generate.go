363462e2cb802a20e8c9d418d308bc3775f15cb6
Alberts Zemzale <alberts.zemzale@gmail.com>
2025-10-15 20:42:10 +0300

    build: Fix broken generate.go

--- generate.go
+++ generate.go
@@ -1,3 +1,3 @@
 package main
 
-//go:generate go tool oapi-codegen -generate types,chi-server -o './pkg/server/server.gen.go' -package server ./spec/openapi.yaml
+//go:generate go tool oapi-codegen -generate types,chi-server -o ./pkg/server/server.gen.go -package server ./spec/openapi.yaml
