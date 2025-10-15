d36e4543288b017ae37d3af548b5e14967dfddac
Alberts Zemzale <alberts.zemzale@gmail.com>
2025-10-15 21:31:35 +0300

    refactor: Abstract fecthing of rates

--- cmd/sync.go
+++ cmd/sync.go
@@ -36,7 +36,7 @@ var syncCmd = &cobra.Command{
 		}
 
 		for _, currency := range allowedCurrencies {
-			if err := syncCurrency(ctx, storage, currency); err != nil {
+			if err := syncCurrency(ctx, storage, currency, LVBankRSSRateFetcher{}); err != nil {
 				return fmt.Errorf("failed to sync currency %s: %w", currency, err)
 			}
 		}
@@ -45,54 +45,17 @@ var syncCmd = &cobra.Command{
 	},
 }
 
-func syncCurrency(ctx context.Context, store *storage.Client, currency string) error {
-	logger := slog.With("component", "sync", "currency", currency)
-	logger.DebugContext(ctx, "Fetching rates")
+type RateFetcher interface {
+	Fetch(ctx context.Context, currency string) (entity.Rate, error)
+}
 
-	url := "https://www.bank.lv/vk/ecb_rss.xml"
-	logger.DebugContext(ctx, "Creating requets for exchange rates", slog.String("url", url))
+func syncCurrency(ctx context.Context, store *storage.Client, currency string, rateFetcher RateFetcher) error {
+	logger := slog.With(slog.String("component", "sync"), slog.String("currency", currency))
 
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
+	rate, err := rateFetcher.Fetch(ctx, currency)
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
+		return fmt.Errorf("failed to fetch rate: %w", err)
 	}
-
-	// TODO: Change out the default client, since it's very bad and has no timeouts
-
-	logger.InfoContext(ctx, "Sending request for exchange rates", slog.String("url", url))
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed to fetch rates: %w", err)
-	}
-	defer resp.Body.Close()
-
-	logger.InfoContext(
-		ctx,
-		"Received response for exchange rates",
-		slog.String("url", url),
-		slog.Int("status", resp.StatusCode),
-	)
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-	}
-
-	logger.DebugContext(ctx, "Parsing rates")
-	rates, err := mapper.RatesFromXML(resp.Body)
-	if err != nil {
-		return fmt.Errorf("failed to parse rates: %w", err)
-	}
-
-	logger.DebugContext(ctx, "Searchign for rate in response", slog.Int("rate_count", len(rates)))
-	idx := slices.IndexFunc(rates, func(r entity.Rate) bool {
-		return r.Code == currency
-	})
-
-	logger.DebugContext(ctx, "Found rate", slog.Int("index", idx))
-	if idx == -1 {
-		return fmt.Errorf("currency %s not found", currency)
-	}
-
-	rate := rates[idx]
 
 	logger.DebugContext(ctx, "Storing rates to database", slog.Any("rate", rate))
 	if err := store.StoreRate(ctx, rate); err != nil {
@@ -107,4 +70,57 @@ func syncCurrency(ctx context.Context, store *storage.Client, currency string) e
 	}
 
 	return nil
+}
+
+// TODO: Maybe just move this to some other package to make it clearer
+type LVBankRSSRateFetcher struct{}
+
+func (f LVBankRSSRateFetcher) Fetch(ctx context.Context, currency string) (entity.Rate, error) {
+	logger := slog.With("component", "LVBankRSSRateFetcher", "currency", currency)
+	logger.DebugContext(ctx, "Fetching rates")
+
+	url := "https://www.bank.lv/vk/ecb_rss.xml"
+	logger.DebugContext(ctx, "Creating requets for exchange rates", slog.String("url", url))
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, http.NoBody)
+	if err != nil {
+		return entity.Rate{}, fmt.Errorf("failed to create request: %w", err)
+	}
+
+	// TODO: Change out the default client, since it's very bad and has no timeouts
+
+	logger.InfoContext(ctx, "Sending request for exchange rates", slog.String("url", url))
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return entity.Rate{}, fmt.Errorf("failed to fetch rates: %w", err)
+	}
+	defer resp.Body.Close()
+
+	logger.InfoContext(
+		ctx,
+		"Received response for exchange rates",
+		slog.String("url", url),
+		slog.Int("status", resp.StatusCode),
+	)
+	if resp.StatusCode != http.StatusOK {
+		return entity.Rate{}, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+
+	logger.DebugContext(ctx, "Parsing rates")
+	rates, err := mapper.RatesFromXML(resp.Body)
+	if err != nil {
+		return entity.Rate{}, fmt.Errorf("failed to parse rates: %w", err)
+	}
+
+	logger.DebugContext(ctx, "Searchign for rate in response", slog.Int("rate_count", len(rates)))
+	idx := slices.IndexFunc(rates, func(r entity.Rate) bool {
+		return r.Code == currency
+	})
+
+	logger.DebugContext(ctx, "Found rate", slog.Int("index", idx))
+	if idx == -1 {
+		return entity.Rate{}, fmt.Errorf("currency %s not found", currency)
+	}
+
+	return rates[idx], nil
 }
